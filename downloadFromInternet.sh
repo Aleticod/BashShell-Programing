11cecb667d62d39972e6daae1e0e359daa0720fc
harveyk <harveyk@harveyk.harveyk>
2022-06-10 04:28:16 -0700

    This code explains how to download a file form internte with wget

---
+++ downloadFromInternet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This program explains how to dawnload information from Internet using wget command
+
+echo "Desacargar Informacion desde Internet"
+
+wget https://downloads.apache.org/tomcat/tomcat-10/v10.1.0-M16/bin/apache-tomcat-10.1.0-M16-deployer.zip
