684015ab223b712da113397e0c5a47681ff96f9f
harveyk <harveyk@harveyk.harveyk>
2022-06-10 00:54:31 -0700

    This is a first challenge

---
+++ utilityHost.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is a challenge when we'l create a two variables
+# and initialize them
+# Author: Harvey Pfoccori
+
+option="Carinio mio"
+result=34
+
+echo "Your option was $option and the restul is $result"
+
