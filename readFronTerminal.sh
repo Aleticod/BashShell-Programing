e1dea9975ea7e1c12ddd4e6b5e3718a4b31a0dc2
harveyk <harveyk@harveyk.harveyk>
2022-06-10 01:02:39 -0700

    This progrma read arguments from terminal or consol

---
+++ readFronTerminal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This program read a arguments from terminal
+# Author: Harvey Pfoccori
+
+echo "What is your name"
+read name
+echo "What is your age"
+read age
+echo "Your name is $name and you are $age years old"
+
